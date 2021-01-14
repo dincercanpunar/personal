400ec237c516a8262284725438809057145fb1da
Dinçer Canpunar <dincercanpunar@hotmail.com>
2021-01-14 12:17:56 +0300

    meta doc updated

--- components/layout/index.js
+++ components/layout/index.js
@@ -51,9 +51,9 @@ function Layout({ children, title }) {
         <meta property="title" content="Dinçer Canpunar | Website" key="title" />
         <meta property="description" content="I’m a senior software developer with experience in developing front-end
         and back-end systems." key="description" />
-        <meta property="og:title" content="Dinçer Canpunar | Website" key="title" />
+        <meta property="og:title" content="Dinçer Canpunar | Website" key="og:title" />
         <meta property="og:description" content="I’m a senior software developer with experience in developing front-end
-        and back-end systems." key="description" />
+        and back-end systems." key="og:description" />
       </Head>
 
       <div className="container">
